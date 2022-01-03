f1091d3782dc9229799020c6ee93b380b378a8c1
Jucelino Novais <jucelinodev@gmail.com>
2022-01-03 13:48:21 -0300

    fix name test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,6 @@
 import { sum } from '.';
 
-describe('Text', () => {
+describe('Sum', () => {
   it('should return the sum of 2 plus 2', async () => {
     const result = sum(2, 2);
     expect(result).toBe(4);
